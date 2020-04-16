dff52869735edfdd3431c7a842ba3c736d1d70f0
oscar sanchez <oscar.sanchez@atmel.com>
2020-04-15 17:18:50 -0700

    fixed error in linker file path

--- xmc1400-getstarted-optiga-trust-m/Debug/optiga-trust-m-lib/optiga/comms/ifx_i2c/subdir.mk
+++ xmc1400-getstarted-optiga-trust-m/Debug/optiga-trust-m-lib/optiga/comms/ifx_i2c/subdir.mk
@@ -4,12 +4,12 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c.c \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_config.c \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.c \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.c \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.c \
-C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.c 
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c.c \
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_config.c \
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.c \
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.c \
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.c \
+C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.c 
 
 OBJS += \
 ./optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c.o \
@@ -29,40 +29,40 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_config.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_config.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_config.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_config.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_data_link_layer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_physical_layer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_presentation_layer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
-optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.c
+optiga-trust-m-lib/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.o: C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/comms/ifx_i2c/ifx_i2c_transport_layer.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: ARM-GCC C Compiler'
-	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m-1/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
+	"$(TOOLCHAIN_ROOT)/bin/arm-none-eabi-gcc" -MMD -MT "$@" -DXMC1404_Q064x0200 -I"$(PROJECT_LOC)/Libraries/XMCLib/inc" -I"$(PROJECT_LOC)/Libraries/CMSIS/Include" -I"$(PROJECT_LOC)/Libraries/CMSIS/Infineon/XMC1400_series/Include" -I"$(PROJECT_LOC)" -I"$(PROJECT_LOC)/Dave/Generated" -I"$(PROJECT_LOC)/Libraries" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/examples/optiga/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/externals/mbedtls/include" -I"C:/Workspace/getstarted-optiga-trust-m/optiga-trust-m/optiga/include" -Os -ffunction-sections -fdata-sections -Wall -std=gnu99 -Wa,-adhlns="$@.lst" -pipe -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d) $@" -mcpu=cortex-m0 -mthumb -g -gdwarf-2 -o "$@" "$<" 
 	@echo 'Finished building: $<'
 	@echo.
 

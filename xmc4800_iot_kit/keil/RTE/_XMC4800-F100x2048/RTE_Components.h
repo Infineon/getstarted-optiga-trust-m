1027109ddcf2604d39cb5f3ef59cb1b4b697c60f
Artem Yushev <12692378+ayushev@users.noreply.github.com>
2021-01-25 22:30:43 +0100

    update source to support trust m3

---
+++ xmc4800_iot_kit/keil/RTE/_XMC4800-F100x2048/RTE_Components.h
@@ -0,0 +1,46 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'xmc4800_optiga_example' 
+ * Target:  'XMC4800-F100x2048' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "XMC4800.h"
+
+#define RTE_DEVICE_STARTUP
+#define RTE_DEVICE_XMCLIB_CAN
+#define RTE_DEVICE_XMCLIB_CCU4
+#define RTE_DEVICE_XMCLIB_CCU8
+#define RTE_DEVICE_XMCLIB_DAC
+#define RTE_DEVICE_XMCLIB_DMA
+#define RTE_DEVICE_XMCLIB_DSD
+#define RTE_DEVICE_XMCLIB_EBU
+#define RTE_DEVICE_XMCLIB_ERU
+#define RTE_DEVICE_XMCLIB_ETH_MAC
+#define RTE_DEVICE_XMCLIB_FCE
+#define RTE_DEVICE_XMCLIB_FLASH
+#define RTE_DEVICE_XMCLIB_GPIO
+#define RTE_DEVICE_XMCLIB_HRPWM
+#define RTE_DEVICE_XMCLIB_I2C
+#define RTE_DEVICE_XMCLIB_I2S
+#define RTE_DEVICE_XMCLIB_LEDTS
+#define RTE_DEVICE_XMCLIB_POSIF
+#define RTE_DEVICE_XMCLIB_RTC
+#define RTE_DEVICE_XMCLIB_SCU
+#define RTE_DEVICE_XMCLIB_SDMMC
+#define RTE_DEVICE_XMCLIB_SPI
+#define RTE_DEVICE_XMCLIB_UART
+#define RTE_DEVICE_XMCLIB_USB
+#define RTE_DEVICE_XMCLIB_VADC
+#define RTE_DEVICE_XMCLIB_WDT
+
+#endif /* RTE_COMPONENTS_H */
